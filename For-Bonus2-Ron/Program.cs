2054a9021d8a3a1a66821a29130211a15ef6ed45
Tarlispace69 <116681079+Tarlispace69@users.noreply.github.com>
2023-10-25 13:27:24 +0300

    For-Bonus1-Ron complete

---
+++ For-Bonus2-Ron/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace For_Bonus2_Ron
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int digit = 0, num = 0, numCopy = 0;
+            bool digitInNum = false;
+
+            Console.Write("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+            Console.Write("Enter a digit: ");
+            digit = int.Parse(Console.ReadLine());
+
+            // checking if digit is in the number
+            numCopy = num;
+            while (!digitInNum && numCopy > 0)
+            {
+                // checking each digit
+                if (numCopy % 10 == digit)
+                {
+                    digitInNum = true;
+                }
+                numCopy /= 10; //movin to next digit
+            }
+
+            //adding the digit to the number if it is not present
+            if (!digitInNum)
+            {
+                num *= 10;
+                num += digit;
+            }
+
+            Console.WriteLine(num);
+            Console.ReadLine();
+        }
+    }
+}
